70ca0248d53ef2c99a50599128988e9cdb945c12
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2024-01-31 19:38:51 -0600

    Ejercicio2

--- Asincronas/S2/Ejercicio2_JhonsonLeiva/src/Main.java
+++ Asincronas/S2/Ejercicio2_JhonsonLeiva/src/Main.java
@@ -2,18 +2,22 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String primerNumero, segundoNumero;
-        int num1, num2, suma;
+        String primerNumero, segundoNumero, tercerNumero;
+        float num1, num2, num3, suma, resta, multiplicacion;
 
         primerNumero = JOptionPane.showInputDialog("Escriba el primer entero: ");
         segundoNumero = JOptionPane.showInputDialog("Escriba el segundo entero: ");
+        tercerNumero = JOptionPane.showInputDialog("Escriba el tercer entero: ");
 
         num1 = Integer.parseInt(primerNumero);
         num2 = Integer.parseInt(segundoNumero);
+        num3 = Integer.parseInt(tercerNumero);
 
-        suma = num1 + num2;
+        suma = num1 + num2 + num3;
+        resta = num1 - num2 - num3;
+        multiplicacion = num1 * num2 * num3;
 
-        JOptionPane.showMessageDialog(null, "la suma es "+ suma, "resultados", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, String.format("La suma es: %.2f\nLa resta es: %.2f\nLa multiplicacion es: %.2f",suma,resta, multiplicacion), "resultados", JOptionPane.PLAIN_MESSAGE);
         System.exit(0);
     }
 }
