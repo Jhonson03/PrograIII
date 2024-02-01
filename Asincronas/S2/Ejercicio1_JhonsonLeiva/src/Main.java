cad8671c48e69a965415c17ace59ed7ba2d29ed2
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2024-01-31 19:54:24 -0600

    Ejercicio1

--- Asincronas/S2/Ejercicio1_JhonsonLeiva/src/Main.java
+++ Asincronas/S2/Ejercicio1_JhonsonLeiva/src/Main.java
@@ -1,10 +1,15 @@
+import javax.swing.*;
 import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola hoy es: ");
-        System.out.println(new Date());
+        String Nom, Apellido;
+
+        Nom = JOptionPane.showInputDialog("Ingrese su nombre ");
+        Apellido = JOptionPane.showInputDialog("Ingrese su apellido ");
+
+        JOptionPane.showMessageDialog(null, String.format("Bienvenido: %s %s",Nom, Apellido));
     }
 }
